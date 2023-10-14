eae4dff15445312d467f3d77cb86479e2daa8613
J-J-T-M <jonatham.junior.18@gmail.com>
2023-10-14 11:12:26 -0400

    AND

--- control_unit.vhd
+++ control_unit.vhd
@@ -25,41 +25,42 @@ ARCHITECTURE control_unit OF control_unit IS
     CONSTANT iSTA : INTEGER := 1;
     CONSTANT iLDA : INTEGER := 2;
     CONSTANT iADD : INTEGER := 3;
-    CONSTANT iOR : INTEGER := 4;
-    CONSTANT iAND : INTEGER := 5;
+    CONSTANT iAND : INTEGER := 5; -- Adicionada a instrução AND
     CONSTANT iNOT : INTEGER := 6;
     CONSTANT iJMP : INTEGER := 7;
     CONSTANT iJN : INTEGER := 8;
     CONSTANT iJZ : INTEGER := 9;
+    CONSTANT iHLT : INTEGER := 15; -- Adicionada a instrução END (HLT)
 
 BEGIN
     carga_REM <= t(0)
-        OR ((t(3) OR t(5)) AND (instr(iLDA) OR instr(iADD) OR instr(iSTA))) OR
+        OR ((t(3) OR t(5)) AND (instr(iLDA) OR instr(iADD) OR instr(iAND))) OR
         (t(3) AND instr(iJMP));
 
     carga_RDM <= t(1)
-        OR ((t(4) OR t(6)) AND (instr(iLDA) OR instr(iADD) OR instr(iSTA))) OR
+        OR ((t(4) OR t(6)) AND (instr(iLDA) OR instr(iADD) OR instr(iAND))) OR
         (t(4) AND instr(iJMP));
 
     inc_PC <= t(1) OR t(2) OR
-        ((t(6) OR t(7)) AND (instr(iLDA) OR instr(iADD) OR instr(iSTA)));
+        ((t(6) OR t(7)) AND (instr(iLDA) OR instr(iADD) OR instr(iAND) OR instr(iHLT)));
 
     carga_RI <= t(2);
 
-    sel <= (t(4) OR t(5)) AND (instr(iLDA) OR instr(iADD) OR instr(iSTA));
+    sel <= (t(4) OR t(5)) AND (instr(iLDA) OR instr(iADD) OR instr(iAND));
 
-    carga_AC <= (t(7) AND (instr(iLDA) OR instr(iADD) OR instr(iSTA))) OR
-        (t(4) AND instr(iOR));
+    carga_AC <= (t(7) AND (instr(iLDA) OR instr(iADD) OR instr(iAND))) OR
+        (t(4) AND instr(iAND));
 
-    carga_NZ <= (t(7) AND (instr(iLDA) OR instr(iADD) OR instr(iSTA))) OR
-        (t(4) AND instr(iOR));
+    carga_NZ <= (t(7) AND (instr(iLDA) OR instr(iADD) OR instr(iAND))) OR
+        (t(4) AND instr(iAND));
 
-    carga_PC <= t(2) OR (t(7) AND (instr(iLDA) OR instr(iADD))) OR
+    carga_PC <= t(2) OR (t(7) AND (instr(iLDA) OR instr(iADD) OR instr(iAND))) OR
         (t(5) AND instr(iJMP));
 
     goto_t0 <= (instr(iNOP) AND t(3)) OR
         (instr(iNOT) AND t(5)) OR
-        (instr(iJMP) AND t(6));
+        (instr(iJMP) AND t(6)) OR
+        (instr(iHLT) AND t(7));
 
     s(0) <= (instr(iADD) AND (t(6) OR t(7))) OR
     (instr(iNOT) AND (t(3) OR t(4)));
